67f5727f4b6fff9972f147f03e50ffd446de7d5e
yamunanavada <yamunanavada@gmail.com>
2017-12-19 15:34:25 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name: "sup1")
+Artist.create(name: "sup2")
+Artist.create(name: "sup3")
+Artist.create(name: "sup4")
+Artist.create(name: "sup5")
+Artist.create(name: "sup6")
+
+Song.create(title: "song1", artist_id: 1)
+Song.create(title: "song2", artist_id: 2)
+Song.create(title: "song3", artist_id: 1)
+Song.create(title: "song14", artist_id: 3)
+Song.create(title: "song15", artist_id: 4)
+Song.create(title: "song6", artist_id: 5)
+Song.create(title: "song7", artist_id: 1)
+Song.create(title: "song02", artist_id: 1)
+Song.create(title: "song1324", artist_id: 1)
